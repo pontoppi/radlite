4e9899078e58e44df94ac357ed39f88f2d350ea4
Jamila Pegues <jpegues@nardaerys.local.stsci.edu>
2019-07-11 13:01:10 -0400

    Starting Radlite and Molecule classes.

---
+++ pyradlite/pyradlite/classMolecule.py
@@ -0,0 +1,16 @@
+
+class Molecule():
+def __init__():
+
+
+class Model_Radlite():
+
+def __init__():
+
+def read_lines() / read_threads():
+
+def genspec():
+
+def read_abundance():
+
+def read_gastemperature():
---
+++ pyradlite/pyradlite/classRadlite.py
@@ -0,0 +1,20 @@
+##FILE:
+##PURPOSE:
+
+
+##
+class Radlite():
+
+def __init__():
+
+def make_abundance():
+
+def make_velocity():
+
+def make_gastemperature():
+
+def make_moldata():
+
+def make_levelpop():
+
+def run():
